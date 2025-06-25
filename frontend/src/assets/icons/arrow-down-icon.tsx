67ec89d4d349a52ee933a4842031f5852b80befe
Олександр <sasha15200201@gmail.com>
2025-06-25 17:15:26 +0300

    offer update, new card

---
+++ frontend/src/assets/icons/arrow-down-icon.tsx
@@ -0,0 +1,7 @@
+export const ArrowDownIcon = () => {
+  return (
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M5 7.5L10 12.5L15 7.5" stroke="#BDF5EF" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  )
+}
